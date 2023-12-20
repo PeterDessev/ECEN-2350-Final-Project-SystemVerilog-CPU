d84cd7737f259570a61ff3a8c00b6ae002c0136a
Peter Dessev <peter.dessev@gmail.com>
2023-12-19 18:50:00 -0700

    Added ALU sim and debugged ALU

---
+++ ecen2350Final.srcs/sources_1/new/ALUSim.sv
@@ -0,0 +1,81 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Ben Jacobsen
+// 
+// Create Date: 12/19/2023 01:44:48 PM
+// Design Name: 
+// Module Name: ALUSim
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALUSim(
+
+    );
+    
+    parameter int bitWidth = 8;
+    parameter int opCodeWidth = 4;
+    parameter int statusWidth = 4;
+    
+    logic[bitWidth-1:0] a, b, q;
+    logic[opCodeWidth-1:0] op;
+    logic[statusWidth-1:0] status;
+    
+    ALU_t #(.BIT_WIDTH(bitWidth), .OP_CODE_WIDTH(opCodeWidth), .STATUS_WIDTH(statusWidth)) dut(.*);
+    
+    initial begin
+    
+        a=0; b=0; op=0; // add
+        #5;
+        
+        a=3; b=2; op=1; // sub
+        #5;
+
+        a=10; b=12; op=2; // inc
+        #5;   
+
+        a=10; b={bitWidth{1'b1}}; op=2; // inc (overflow)
+        #5;  
+
+        a=10; b=12; op=3; // dec
+        #5;   
+        
+        a=10; b=1; op=3; // dec (zero)
+        #5;   
+
+        a=10; b=0; op=3; // dec (uderflow)
+        #5;   
+
+        a=8'b00001111; b=8'b10101010; op=4; // and
+        #5;
+
+        a=8'b00001111; b=8'b10101010; op=5; // or
+        #5;
+
+        a=8'b00001111; b=8'b10101010; op=6; // not
+        #5;
+
+        a=8'b00001111; b=8'b10101010; op=7; // xor
+        #5;
+
+        a=8'b00001111; b=8'b10101010; op=8; // shl
+        #5;
+
+        a=8'b00001111; b=8'b10101010; op=9; // shr
+        #5;
+    
+        //Most of origonal test cases seen in TD were lost in a previoud file.
+    
+    end
+endmodule
--- ecen2350Final.srcs/sources_1/new/ALU_t.sv
+++ ecen2350Final.srcs/sources_1/new/ALU_t.sv
@@ -34,7 +34,7 @@ module ALU_t #(parameter BIT_WIDTH=8,
     // logic additionCarry;
 
     // rca #(.BIT_WIDTH(BIT_WIDTH)) adder(.a(a), .b(b), .cin(1'b0), .q(additionOut), .cout(additionCarry));
-
+    // assign status = (q == 0) && 4'n00
     always_comb
         case (op)
             4'b0000: begin  // add
@@ -47,27 +47,55 @@ module ALU_t #(parameter BIT_WIDTH=8,
             end
             4'b0010: begin // inc
                 q = b + 1;
-                status = b + 1 == 0 ? 4'b0001 : 4'b0000;
+                status = b == {BIT_WIDTH{1'b1}} ? 4'b0001 : 4'b0000;
             end
             4'b0011: begin // dec
                 q = b - 1;
-                status = b - 1 == 0 ? 4'b0010 : 4'b0000;
+                status = b == 1'b0 ? 4'b0001 : b == 1'b1 ? 4'b0010 : 4'b0000;
             end
-            4'b0100: // and
+            4'b0100: begin // and
                 q = a & b;
-            4'b0101: // or
+                status = q == 1'b0 ? 4'b0010 : 4'b0000;
+            end
+            4'b0101: begin // or
                 q = a | b;
-            4'b0110: // not
+                status = q == 1'b0 ? 4'b0010 : 4'b0000;
+            end
+            4'b0110: begin // not
                 q = ~b;
-            4'b0111: // xor
+                status = q == 1'b0 ? 4'b0010 : 4'b0000;
+            end
+            4'b0111: begin // xor
                 q = a ^ b;
-            4'b1000: // shl
+                status = q == 1'b0 ? 4'b0010 : 4'b0000;
+            end
+            4'b1000: begin // shl
                 q = {b[BIT_WIDTH - 2:0], 1'b0};
+
+                if (b == 0) begin
+                    status = 4'b0010;
+                end else if (b[BIT_WIDTH - 1]) begin
+                    status = 4'b0001;
+                end else begin
+                    status = 0;
+                end
                 // q <= a << b;
-            4'b1001: // shr
+            end
+            4'b1001: begin // shr
                 // q <= a >> b;
                 q = {1'b0, b[BIT_WIDTH - 1:1]};
+                
+                if (b == 0) begin
+                    status = 4'b0010;
+                end else if (b[0]) begin
+                    status = 4'b0001;
+                end else begin
+                    status = 0;
+                end
+            end
             default: begin
+                q = 0;
+                status = 0;
             end
         endcase
 endmodule
