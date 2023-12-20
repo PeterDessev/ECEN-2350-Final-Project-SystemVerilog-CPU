39ad652c4cbfa0d90fa2dbe294e70cd6b40be978
Peter Dessev <peter.dessev@gmail.com>
2023-12-19 22:03:39 -0700

    Memory debug and initialization

--- ecen2350Final.srcs/sources_1/new/memory_t.sv
+++ ecen2350Final.srcs/sources_1/new/memory_t.sv
@@ -21,7 +21,8 @@
 
 
 module memory_t #(parameter BUS_WIDTH=8,
-                  parameter NUM_BYTES=(2 ** BUS_WIDTH - 1)) (
+                  parameter NUM_BYTES=(2 ** BUS_WIDTH - 1),
+                  parameter MEM_INIT_FILE = ""  ) (
     input [BUS_WIDTH - 1:0] address,
     input [BUS_WIDTH - 1:0] writeData,
     input writeEnable,
@@ -31,8 +32,17 @@ module memory_t #(parameter BUS_WIDTH=8,
 
     logic [7:0]memoryCell[NUM_BYTES : 0];
 
+    initial begin
+        if (MEM_INIT_FILE != "") begin
+            $readmemh(MEM_INIT_FILE, memoryCell);
+        end
+    end
+
     assign readData = memoryCell[address];
     always @(posedge(clk)) begin
         memoryCell[address] <= writeEnable ? writeData : memoryCell[address];
+        if(writeEnable) begin
+            $display("Memory: Writing %h to address %h", writeData, address);
+        end
     end
 endmodule
